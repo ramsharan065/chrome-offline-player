b6bd01828c9e0c09ac8b9e638bb0cd5ead1c002a
Ramsharan Shrestha <ramsharan065@gmail.com>
2016-11-28 00:34:35 +0545

    Protect dinosaur from birds #1
    1. Check for bird and go down to avoid strike from bird
    2. Use up and down key instead of space

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function Player () {
 }
 
 Player.prototype.jump = function () {
-	var k = 32;
+	var k = 38;
 	var oEvent = new KeyboardEvent('keydown', {
 		keyCode: k,
 		which: k
@@ -21,18 +21,73 @@ Player.prototype.jump = function () {
     });
     document.dispatchEvent(oEvent);
 }
+
+Player.prototype.down = function () {
+	var k = 40;
+	var oEvent = new KeyboardEvent('keydown', {
+		keyCode: k,
+		which: k
+	});
+
+    // Chromium Hack
+    Object.defineProperty(oEvent, 'keyCode', {
+        get : function() {
+            return k;
+        }
+    });     
+    Object.defineProperty(oEvent, 'which', {
+        get : function() {
+            return k;
+        }
+    });
+    document.dispatchEvent(oEvent);
+}
+
+Player.prototype.up = function () {
+	var k = 40;
+	var oEvent = new KeyboardEvent('keyup', {
+		keyCode: k,
+		which: k
+	});
+
+    // Chromium Hack
+    Object.defineProperty(oEvent, 'keyCode', {
+        get : function() {
+            return k;
+        }
+    });     
+    Object.defineProperty(oEvent, 'which', {
+        get : function() {
+            return k;
+        }
+    });
+    document.dispatchEvent(oEvent);
+}
+
 Player.prototype.run = function() {
 	var that = this;
 	var a = setInterval(function(){
 		if (that.shouldJump()) {
 			that.jump();		
-		};
+		} else if (that.shouldDown()) {
+			that.down();
+			setTimeout(function(){
+				var t = that;
+				t.up();
+			},500);
+		}
 	}, 100);		
 }
 Player.prototype.shouldJump = function() {
 	var nearestObjectDistance = getNearestObjectDistance();
 	console.log(nearestObjectDistance);
 	return nearestObjectDistance > 0 && nearestObjectDistance < 80;
+}
+
+Player.prototype.shouldDown = function() {
+	var nearestBirdtDistance = getNearestBirdDistance();
+	console.log(nearestBirdtDistance);
+	return nearestBirdtDistance > 0 && nearestBirdtDistance < 80;
 }
 
 function getNearestObjectDistance(){
@@ -58,6 +113,31 @@ function getNearestObjectDistance(){
 		}
 	}
 	return nearestObjectDistance;
+}
+
+function getNearestBirdDistance(){
+	var nearestBirdDistance = 0;
+
+	var playground = document.getElementsByTagName('canvas')[0];
+	var playgroundWidth = playground.width;
+	var playgroundHeight = playground.height;
+	var context = playground.getContext("2d");
+	var imageData = context.getImageData(0,0,playgroundWidth,playgroundHeight);
+	var data = imageData.data;
+
+	var startHorizontal = 70;
+	var startVertial = 85;
+	var currentIndex;
+	var color;
+	for (var i = startHorizontal; i < playgroundWidth; i++) {
+		currentIndex = (startVertial*playgroundWidth*4) + i*4;
+		color = data[currentIndex];
+		if (color > 50) {
+			nearestBirdDistance = i - startHorizontal;
+			break;
+		}
+	}
+	return nearestBirdDistance;
 }
 
 
